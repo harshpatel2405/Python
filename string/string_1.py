188e6e7600dda235d550d208e92f8e7fe11f861d
harshpatel2405 <harshpatel4504@gmail.com>
2026-07-01 13:26:22 +0530

    string

--- string/string_1.py
+++ string/string_1.py
@@ -36,7 +36,7 @@ print(name.lower())
 # * title -- capitalize first letter of each word
 print(str.title())
 
-# * capitalize -- capitailze the first character of the string 
+# * capitalize -- capitailze the first character of the string
 print(str.capitalize())
 
 
@@ -59,3 +59,45 @@ print(str.capitalize())
 11. Write a program to count the number of characters in a user-entered string.
 12. Write a program that converts a user's name to uppercase.
 '''
+'''7. Store the string "machine learning" and print:
+    Original string
+    Uppercase
+    Lowercase
+    Title case
+    Capitalized
+'''
+str = "machine learning"
+print("Original String :", str)
+print("Uppercase String :", str.upper())
+print("Lowercase String :", str.lower())
+print("Title String :", str.title())
+print("Capitalized String :", str.capitalize())
+
+
+'''
+8. Write a program to print the first and last character of a string entered by the user.
+'''
+str = input("Enter a string : ")
+print(str[0])
+print(str[-1])
+
+'''
+9. Write a program to concatenate two strings entered by the user.
+'''
+str1 = "harsh"
+str2 = " patel"
+print(str1+str2)
+
+
+'''
+10. Write a program to print a string 10 times using the repetition operator.
+'''
+str = "task"
+print(str*10)
+
+'''
+11. Write a program to count the number of characters in a user-entered string. -- len 
+12. Write a program that converts a user's name to uppercase.
+'''
+str = input("Enter your name : ")
+print(str.upper())
